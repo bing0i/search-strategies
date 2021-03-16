5e8f4821cb0810edb39b2f2c0e53f5e36a75a7b0
Courgette <courgett3@gmail.com>
2021-03-16 19:52:48 +0700

    Add writeOutputFile() and fix a bug in (while loop condition) BFS

--- main.py
+++ main.py
@@ -13,6 +13,28 @@ def readInputFile(path):
     return data
 
 
+def writeOutputFile(path, output):
+    with open(path, "w") as outputFile:
+        outputFile.write(" ".join([str(i) for i in output["expandedNodes"]]) + "\n")
+        if type(output["path"]) == type(""):
+            outputFile.write(output["path"])
+        else:
+            outputFile.write(" ".join([str(i) for i in output["path"]]))
+    return True
+
+
+def getPath(parentsOfNodes, destinationIndex):
+    path = [destinationIndex]
+    currentParents = parentsOfNodes[destinationIndex]
+    path.append(currentParents[-1])
+    while currentParents:
+        currentParents = parentsOfNodes[path[-1]]
+        if currentParents:
+            path.append(currentParents[-1])
+    path.reverse()
+    return path
+
+
 def BFS(data):
     queue = [data["sourceIndex"]]
     expandedNodes = []
@@ -21,7 +43,7 @@ def BFS(data):
     for i in range(data["N"]):
         parentsOfNodes[i] = []
 
-    while len(queue) >= 0:
+    while len(queue) > 0:
         currentNode = queue.pop(0)
         expandedNodes.append(currentNode)
         for node, weight in enumerate(data["matrix"][currentNode]):
@@ -47,17 +69,5 @@ def BFS(data):
     return output
 
 
-def getPath(parentsOfNodes, destinationIndex):
-    path = [destinationIndex]
-    currentParents = parentsOfNodes[destinationIndex]
-    path.append(currentParents[-1])
-    while currentParents:
-        currentParents = parentsOfNodes[path[-1]]
-        if currentParents:
-            path.append(currentParents[-1])
-    path.reverse()
-    return path
-
-
-inp = readInputFile("input.txt")
-print(BFS(inp))
+data = readInputFile("input.txt")
+writeOutputFile("output.txt", BFS(data))
