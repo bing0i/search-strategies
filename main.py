2d89b89acd3386f93c10acb4b4b429f7d8b151f1
Courgette <courgett3@gmail.com>
2021-03-16 19:28:37 +0700

    Complete BreathFirstSearch and getPath (for all strategies)

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
 def readInputFile(path):
-    input = {}
+    data = {}
     with open(path, "r") as inputFile:
-        input["N"] = int(inputFile.readline())
+        data["N"] = int(inputFile.readline())
         tmpNextLine = inputFile.readline()
-        input["sourceIndex"] = int(tmpNextLine[0])
-        input["destinationIndex"] = int(tmpNextLine[2])
-        input["searchStrategy"] = int(tmpNextLine[4])
-        input["matrix"] = []
-        for i in range(input["N"]):
-            input["matrix"] = input["matrix"] + inputFile.readline().split()
-        input["heuristicValues"] = inputFile.readline().split()
-    return input
+        data["sourceIndex"] = int(tmpNextLine[0])
+        data["destinationIndex"] = int(tmpNextLine[2])
+        data["searchStrategy"] = int(tmpNextLine[4])
+        data["matrix"] = [
+            [int(i) for i in inputFile.readline().split()] for _ in range(data["N"])
+        ]
+        data["heuristicValues"] = [int(i) for i in inputFile.readline().split()]
+    return data
 
 
-print(readInputFile("input.txt"))
+def BFS(data):
+    queue = [data["sourceIndex"]]
+    expandedNodes = []
+
+    parentsOfNodes = {}
+    for i in range(data["N"]):
+        parentsOfNodes[i] = []
+
+    while len(queue) >= 0:
+        currentNode = queue.pop(0)
+        expandedNodes.append(currentNode)
+        for node, weight in enumerate(data["matrix"][currentNode]):
+            if weight != 0 and node not in queue and node not in expandedNodes:
+                if node == data["destinationIndex"]:
+                    parentsOfNodes[node].append(currentNode)
+                    output = {
+                        "queue": queue,
+                        "expandedNodes": expandedNodes,
+                        "parentsOfNodes": parentsOfNodes,
+                        "path": getPath(parentsOfNodes, data["destinationIndex"]),
+                    }
+                    return output
+                queue.append(node)
+                parentsOfNodes[node].append(currentNode)
+
+    output = {
+        "queue": queue,
+        "expandedNodes": expandedNodes,
+        "parentsOfNodes": parentsOfNodes,
+        "path": "No path",
+    }
+    return output
+
+
+def getPath(parentsOfNodes, destinationIndex):
+    path = [destinationIndex]
+    currentParents = parentsOfNodes[destinationIndex]
+    path.append(currentParents[-1])
+    while currentParents:
+        currentParents = parentsOfNodes[path[-1]]
+        if currentParents:
+            path.append(currentParents[-1])
+    path.reverse()
+    return path
+
+
+inp = readInputFile("input.txt")
+print(BFS(inp))
