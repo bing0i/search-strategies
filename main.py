4b2608891639613315ab1da1160d8b12dfe6a8bf
Courgette <courgett3@gmail.com>
2021-03-16 12:08:15 +0700

    readInputFile() is completed

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-msg = "Hello World"
-print(msg)
+def readInputFile(path):
+    input = {}
+    with open(path, "r") as inputFile:
+        input["N"] = int(inputFile.readline())
+        tmpNextLine = inputFile.readline()
+        input["sourceIndex"] = int(tmpNextLine[0])
+        input["destinationIndex"] = int(tmpNextLine[2])
+        input["searchStrategy"] = int(tmpNextLine[4])
+        input["matrix"] = []
+        for i in range(input["N"]):
+            input["matrix"] = input["matrix"] + inputFile.readline().split()
+        input["heuristicValues"] = inputFile.readline().split()
+    return input
+
+
+print(readInputFile("input.txt"))
